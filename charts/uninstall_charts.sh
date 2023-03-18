2fd6a1927b4d22249cf8802d47f3309a0ed2244a
Ivan <isolis@solavcr.com>
2023-03-17 23:31:47 -0600

    all databases cluster working

--- charts/uninstall_charts.sh
+++ charts/uninstall_charts.sh
@@ -3,6 +3,7 @@ helm uninstall databases-v1
 helm uninstall grafana-config-v1 
 helm uninstall monitoring-stack-v1 
 helm uninstall app-v1
+sh app/uninstall_flask_app.sh
 minikube kubectl delete pvc data-databases-v1-mariadb-galera-0
 minikube kubectl delete pvc data-databases-v1-mariadb-galera-1
 minikube kubectl delete pvc data-databases-v1-mariadb-galera-2
